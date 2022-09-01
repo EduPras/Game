f4f05cc1843550c459494cd0b68044d72f45ded4
Eduardo Prasniewski <eduardoprasniewski@gmail.com>
2022-09-01 12:34:19 -0300

    Controlling item creation

--- game/libGame/item/item.hpp
+++ game/libGame/item/item.hpp
@@ -3,15 +3,33 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 class Item {
-  static int no_items;
+  static vector<Item*> all_items_;
 protected:
   string name_;
   Item(string s): name_(s) {
-    no_items++;
+    vector<Item*>::iterator it;
+    stringstream ss;
+    if(all_items_.empty()) {
+      all_items_.push_back(this);
+    }
+    else{
+      for(it = all_items_.begin(); it != all_items_.end(); it++){
+        if((*it)->name_ == s){
+          cout << (*it)->name_ << " -> " << s << endl;
+          ss << "Unable to create item: \"" << s << "\"(Its name already exists)\n";
+          throw ss.str();
+          break;
+        }
+      }
+      cout << "Creating Item...\n";
+      all_items_.push_back(this);
+    }
   };
 public:
   ~Item(){
@@ -22,7 +40,7 @@ public:
   };
   virtual string getClass() = 0;
   virtual string describe() = 0;
-  static int getNumberOfItems(){ return no_items; }
+  static int getNumberOfItems(){ return all_items_.size(); };
 };
 
 
--- game/main.cpp
+++ game/main.cpp
@@ -6,20 +6,25 @@
 
 using namespace std;
 
-int Item::no_items = 0;
+vector<Item*> Item::all_items_ = {};
 
 int main(){
-  vector<Item*> items;
-  items.push_back(new Sword(10, 0.9, "Ice"));
-  items.push_back(new Rod(10, 0.7, "Colossus"));
-  items.push_back(new Rock("Death ring", 5, 10));
-  Character c = Warrior("Garrosh");
-  Character w = Wizard("Guldan");
-  c << (items[0]);
-  c << (items[2]);
-  c.show_status();
-  w.show_status();
-  w.attack(c, (Weapon *)items[1]);
-  cout << Item::getNumberOfItems() << " items\n";
+  try{
+    vector<Item*> items;
+    items.push_back(new Sword(10, 0.9, "Ice"));
+    items.push_back(new Sword(10, 0.8, "Ice"));
+    items.push_back(new Rod(10, 0.7, "Colossus"));
+    items.push_back(new Rock("Death ring", 5, 10));
+    Character c = Warrior("Garrosh");
+    Character w = Wizard("Guldan");
+    c << (items[0]);
+    c << (items[2]);
+    c.show_status();
+    w.show_status();
+    w.attack(c, (Weapon *)items[1]);
+    cout << Item::getNumberOfItems() << " items\n";
+  }catch(string msg){
+    cout << msg;
+  }
   return 0;
 }
